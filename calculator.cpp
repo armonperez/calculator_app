8805dee6f9849a6e5044437d76c03f9e87f53cac
armonperez <106364512+armonperez@users.noreply.github.com>
2023-08-12 16:12:50 -0700

    restructured to include op to store the operator, and the two numbers stored in num1 and num2 variables. Case is used for each operator do execute if it is used. Default is used to execute an error if none of the operators are used.

--- calculator.cpp
+++ calculator.cpp
@@ -6,18 +6,38 @@ using namespace std; // directive
 
 int main ()
 {
-    int a;
-    int b;
-    int sum;
+   char op;
+   float num1, num2;
 
-    cout << "Enter a number \n";
-    cin >> a;
+   cout << "Enter operator: +, -, *, /:";
+   cin >> op;
 
-    cout << "Enter another number \n";
-    cin >> b;
+   cout << "Enter two numbers:";
+   cin >> num1 >> num2;
 
-    sum = a + b;
-    cout << "the sum of those numbers is " << sum << endl;
+    switch (op) {
+        case  '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2;
+            break;
+
+        default:
+            // If the operator is other than +, -, * or /, error message is shown
+            cout << "Error! operator is not correct";
+            break;
+
+    }
 
 
 
