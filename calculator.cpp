aa81196545575ab0014f4c29de61754ce95f1ba9
armonperez <106364512+armonperez@users.noreply.github.com>
2023-08-12 15:43:00 -0700

    Added addition function
    added the addition function to start adding two numbers and outputting the sum.

---
+++ calculator.cpp
@@ -0,0 +1,26 @@
+//
+// Created by armon on 8/12/2023.
+
+#include <iostream> //libary
+using namespace std; // directive
+
+int main ()
+{
+    int a;
+    int b;
+    int sum;
+
+    cout << "Enter a number \n";
+    cin >> a;
+
+    cout << "Enter another number \n";
+    cin >> b;
+
+    sum = a + b;
+    cout << "the sum of those numbers is " << sum << endl;
+
+
+
+return 0;
+
+}
